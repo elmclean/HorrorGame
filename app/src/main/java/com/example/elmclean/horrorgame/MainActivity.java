2acddccc5bfa3de08917b003e22feae5f0b34840
elmclean <elmclean@svsu.edu>
2016-10-12 15:18:43 -0400

    Initial commit

---
+++ app/src/main/java/com/example/elmclean/horrorgame/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.elmclean.horrorgame;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Typeface type = Typeface.createFromAsset(getAssets(),"fonts/umeplus-gothic.ttf");
+        TextView titleText = (TextView) findViewById(R.id.title);
+        Button newGame = (Button) findViewById(R.id.new_game);
+        Button resume = (Button) findViewById(R.id.resume);
+        Button quit = (Button) findViewById(R.id.quit);
+
+        titleText.setTypeface(type);
+        newGame.setTypeface(type);
+        resume.setTypeface(type);
+        quit.setTypeface(type);
+    }
+}
