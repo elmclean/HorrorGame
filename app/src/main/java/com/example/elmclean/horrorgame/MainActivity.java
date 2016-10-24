c944ac38e978ed3709b826c4337e8bb202bfbf3d
elmclean <elmclean@svsu.edu>
2016-10-24 16:11:32 -0400

    added title background music

--- app/src/main/java/com/example/elmclean/horrorgame/MainActivity.java
+++ app/src/main/java/com/example/elmclean/horrorgame/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.elmclean.horrorgame;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.graphics.Typeface;
+import android.os.AsyncTask;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnErrorListener;
+import android.os.Binder;
+import android.os.IBinder;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,12 +36,16 @@ public class MainActivity extends AppCompatActivity {
         titleText.setTypeface(type);
         newGame.setTypeface(type);
         resume.setTypeface(type);
+
+        Intent svc = new Intent(this, MusicService.class);
+        svc.setAction("com.example.MusicService");
+        startService(svc);
     }
 
     public void startGame(View v)
     {
+        stopService(new Intent(MainActivity.this,MusicService.class));
         Intent intent = new Intent(this, StartGame.class);
         startActivity(intent);
     }
-
 }
---
+++ app/src/main/java/com/example/elmclean/horrorgame/MusicService.java
@@ -0,0 +1,57 @@
+package com.example.elmclean.horrorgame;
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnErrorListener;
+import android.os.Binder;
+import android.os.IBinder;
+import android.widget.Toast;
+
+/**
+ * Created by elmclean on 10/24/2016.
+ */
+
+public class MusicService extends Service {
+    private static final String TAG = null;
+    MediaPlayer player;
+    public IBinder onBind(Intent arg0) {
+
+        return null;
+    }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        player = MediaPlayer.create(this, R.raw.lost_chair);
+        player.setLooping(true);
+        player.setVolume(100,100);
+
+    }
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        player.start();
+        return 1;
+    }
+
+    public void onStart(Intent intent, int startId) {
+
+    }
+    public IBinder onUnBind(Intent arg0) {
+        return null;
+    }
+
+    public void onStop() {
+
+    }
+    public void onPause() {
+
+    }
+    @Override
+    public void onDestroy() {
+        player.stop();
+        player.release();
+    }
+
+    @Override
+    public void onLowMemory() {
+
+    }
+}
